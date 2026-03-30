7e6b77378e13a58060eedca5bbebba1ea4f40bf3
Brendon W. <bwherry@midland.k12.ia.us>
2026-03-30 12:06:08 -0500

    Fix CreateTeam method to add string literals

--- Classroom.cs
+++ Classroom.cs
@@ -4,9 +4,9 @@ public class Classroom{
 
     public HashSet<string> CreateTeam(string first, string second, string third){
         HashSet<string> team = new HashSet<string>();
-            team.Add(apple);
-            team.Add(pear);
-            team.Add(banana);
+            team.Add("apple");
+            team.Add("pear");
+            team.Add("banana");
         
         return team;
     }
